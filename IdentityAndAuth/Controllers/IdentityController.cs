625cb27e7c8e6d747a3675040fa0362234d58cef
pavan <mani365@gmail.com>
2024-02-16 10:15:56 +0530

    setting up signup and signin in identity controller and created signup class in models

---
+++ IdentityAndAuth/Controllers/IdentityController.cs
@@ -0,0 +1,29 @@
+﻿using IdentityAndAuth.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAndAuth.Controllers
+{
+    public class IdentityController : Controller
+    {
+        public async Task<IActionResult>SignUp()
+        {
+            var model = new SignUp();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUp model)
+        {
+            return View(model);
+        }
+
+        public async Task<IActionResult> SignIn()
+        {
+            return View();
+        }
+        public async Task<IActionResult> AccessDenied()
+        {
+            return View();
+        }
+    }
+}
---
+++ IdentityAndAuth/Models/SignUp.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityAndAuth.Models
+{
+    public class SignUp
+    {
+        [Required]
+        [DataType(DataType.EmailAddress,ErrorMessage="Email is missing")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password,ErrorMessage ="Incorrect or missing password")]
+        public string Password { get; set; }
+    }
+}
