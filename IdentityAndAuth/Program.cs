42a13d92b70963f1342e7c543da600398c5ec37c
pavan <mani365@gmail.com>
2024-02-16 09:39:33 +0530

    setting up cookie based auth

--- IdentityAndAuth/Program.cs
+++ IdentityAndAuth/Program.cs
@@ -4,11 +4,32 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//adding conn string
 var connectionString = builder.Configuration["ConnectionStrings:Default"];
-
 builder.Services.AddDbContext<ApplicationDbContext>(o=>o.UseSqlServer(connectionString));
+//adding identity
 builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
+//cookie based auth
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = true; 
+    options.Password.RequireDigit=true; 
+    options.Password.RequireLowercase = true; 
+    options.Password.RequireUppercase = true;
+    options.SignIn.RequireConfirmedEmail = true;
+
+});
+
+//to enable coikie based auth
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Home/Signin";
+    options.AccessDeniedPath = "/Home/AccessDeniedPath";
+
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
